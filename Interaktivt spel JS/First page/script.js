d129a6e1b9afdb0710d0bff77ab5342c687334cc
AdrianRydin <adrian.rydin@gmail.com>
2021-11-19 15:14:42 +0100

    Fixade en till scen, lade även till en README fil

--- Interaktivt spel JS/First page/script.js
+++ Interaktivt spel JS/First page/script.js
@@ -1,14 +1,40 @@
 
+window.onload = main();
+
+function main(){
+  document.getElementById('html');
+  document.getElementsByClassName('main');
+
+}
+
+/**Ger användaren ett username */
+
 // const userName = prompt("Hej! Välkommen till mitt interaktiva spel, vänligen ange ditt namn");
 
 // console.log(userName);
 
-function helpButtonClick(){
 
+const pages = {
+  "firstDownStairs": document.getElementById('firstDownStairs')
+
+}
+const items = {
+
+  "weapon": document.getElementById("baseballBat"),
+  "stairsImage": document.getElementById("staircase"),
+  "firstDownStairsButton": document.getElementById("firstButton"),
+  "weaponButton": document.getElementById('secondButton'),
+  "storyText1": document.getElementById('storyText'),
+  "storyText2": document.getElementById('storyText2'),
+  "hungryImage": document.getElementById('hungryImage')
+  
+}
+
+/**Dropdown meny uppe till höger */
+function helpButtonClick(){
 
     document.getElementById("dropdown").classList.toggle("show");
 
-    
 }
 
 window.onclick = function(event) {
@@ -24,13 +50,28 @@ window.onclick = function(event) {
     }
   }
 
+/** 
+ * Början av spelet, byter scen när man trycker på knappen med trapporna. 
+*/
 
+ function downStairs(){
+  items.weapon.style.display = "none"
+  items.weaponButton.style.display = "none"
+  items.stairsImage.style.display = "none"
+  items.firstDownStairsButton.style.display = "none"
+  items.storyText1.style.display = "none"
+  document.getElementById('subTitle').style.display = "none"
+  pages.firstDownStairs.style.display = "flex"
+ }
 
- function nextScene(){
-    
+ function springButton(){alert('Du dog!')}
 
-    console.log("YO")
+ function weapon(){
 
-    
-     
+  items.weapon.style.display = "none"
+  items.weaponButton.style.display = "none"
+  items.stairsImage.style.display = "none"
+  items.firstDownStairsButton.style.display = "none"
+  items.storyText1.style.display = "none"
+  document.getElementById('subTitle').style.display = "none"
  }
